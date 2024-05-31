3882c028bbde0178eda503ddee0b813defae2dc4
joshuajong1 <joshua.jong@talenox.com>
2024-05-31 11:26:42 +0800

    Create basic chatbot

---
+++ chatbot/chatbot.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+import json
+
+class ChatBot:
+  def __init__(self, knowledge_source, api_key, general_context):
+    # Load the knowledge source
+    with open(knowledge_source, 'r') as f:
+      self.knowledge_base = json.load(f)["knowledge_base"]
+
+    # Initialise the open ai client
+    self.client = OpenAI(
+      api_key=api_key,
+    )
+    # Model should contain some general context in case knowledge base is insufficient to answer the user's query
+    self.general_context = general_context
+
+  def search_knowledge_base_for_best_match(self, user_input):
+    for context in self.knowledge_base:
+      for question, answer in context["qa_pairs"].items():
+        if user_input.lower() in question.lower():
+          return answer
+    return None
+  
+  def generate_response(self, user_prompt):
+    search_knowledge_base_for_best_match = self.search_knowledge_base_for_best_match(user_prompt)
+    if search_knowledge_base_for_best_match:
+      return search_knowledge_base_for_best_match
+    else:
+      prompt = self.general_context + "\n\nUser: " + user_prompt + "\n\Customer Support: "
+      response = self.client.chat.completions.create(
+        messages=[
+          {
+            "role": "user",
+            "content": prompt,
+            "stop": ["User:", "Customer Support:"],
+          }
+        ],
+        model="gpt-3.5-turbo",
+      )
+    return response.choices[0].message.content
--- main.py
+++ main.py
@@ -1,2 +1,35 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import csv
+from chatbot.chatbot import *
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the OpenAI API key from environment variables
+api_key = os.environ.get("OPENAI_API_KEY")
+if not api_key:
+  raise ValueError("OpenAI API key is not set. Please check your .env file.")
+
+knowledge_source = './knowledgebase/sample_data.json'
+general_context = "We are a customer support service for an online retail store. We handle inquiries related to our return policy, shipping information, product details, and other customer service issues."
+
+chatbot = ChatBot(knowledge_source, api_key, general_context)
+
+def main():
+  print("Welcome to the Customer Support Chatbot!")
+  print("Type 'exit' to end the conversation.")
+  
+  while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+      print("Chatbot: Thank you for using our customer support. Have a great day!")
+      break
+    
+    response = chatbot.generate_response(user_input)
+    print(f"Chatbot: {response}")
+
 if __name__ == "__main__":
-  print("Hello world!")
+  main()

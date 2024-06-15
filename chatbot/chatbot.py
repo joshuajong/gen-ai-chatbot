e808e8b295bed0415a7a6f22c0d750e02e23550f
joshuajong1 <joshua.jong@talenox.com>
2024-06-15 18:01:48 +0800

    Use v3 client

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 import json
 
 class ChatBot:
-  def __init__(self, knowledge_source, api_key, general_context):
+  def __init__(self, knowledge_source, api_key, general_context, weaviate_client):
     # Load the knowledge source
     with open(knowledge_source, 'r') as f:
       self.knowledge_base = json.load(f)["knowledge_base"]
@@ -14,12 +14,26 @@ class ChatBot:
     # Model should contain some general context in case knowledge base is insufficient to answer the user's query
     self.general_context = general_context
 
+    # Initialise weaviate client for vector search
+    self.weaviate_client = weaviate_client
+
   def search_knowledge_base_for_best_match(self, user_input):
     for context in self.knowledge_base:
       for question, answer in context["qa_pairs"].items():
         if user_input.lower() in question.lower():
           return answer
     return None 
+  
+  def search_weaviate_for_best_match(self, user_input):
+    query = {
+            "concepts": [user_input],
+            "certainty": 0.7
+    }
+    response = self.weaviate_client.query.get("Document", ["content"]).with_near_text(query).do()
+    
+    if response['data']['Get']['Document']:
+      return response['data']['Get']['Document'][0]['content']
+    return "No additional information found."
   
   def generate_response(self, user_prompt):
     search_knowledge_base_for_best_match = self.search_knowledge_base_for_best_match(user_prompt)
